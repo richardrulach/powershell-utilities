f9d5a29e0599c1a5cc00d7ddaef52d824be1c92a
Richard Rulach <richardrulach@gmail.com>
2019-07-17 07:01:20 +0100

    added error checks and tests

--- modules/ProjectTools/function-New-Ticket.ps1
+++ modules/ProjectTools/function-New-Ticket.ps1
@@ -7,28 +7,48 @@ function New-Ticket {
           [Parameter(Mandatory=$true)]
           [String[]] $Ticket
         , [String]   $Description
-        , [String]   $Type
-        , [switch]   $Rebuild
+        , [String]   $Type = 'all'
         , [String]   $Path = "c:\DEV"
+        , [switch]   $Rebuild
+        , [switch]   $Force
 
     )
 
     BEGIN {}
 
     PROCESS{
-        $output = @{}
-        $output["sql"] = "No"
-        $output["ssis"] = "No"
-        $output["test"] = "No"
+
+
+        #region VALIDATION
+        if (-not [System.IO.Directory]::Exists($Path)){ throw "Path does not exist" }
+
+        foreach ($ticketNumber in $Ticket){
+            if ((gci "$($Path)\$($ticketNumber)*").Count -gt 0 -and $Force -eq $false) { 
+                throw "Ticket folder already exists - use force to create anyway"
+            }
+        } 
+        #endregion VALIDATION
+
+
+        #region MAIN TICKET LOOP
+        foreach ($ticketNumber in $Ticket){
+
+            $output = @{}
+            $output["sql"] = "No"
+            $output["ssis"] = "No"
+            $output["test"] = "No"
+            $output["errorMessage"] = ""
+            $output["Path"] = $Path
     
-        try {
+            if ($type.ToLower() -eq 'sql' -or $type -eq 'all'){
+                
+            
 
-        } catch {
+            }
 
-        } finally {
             Write-Output $output    
-        }
 
+        }
     }
 
     END {}
--- modules/ProjectTools/function-New-Ticket.tests.ps1
+++ modules/ProjectTools/function-New-Ticket.tests.ps1
@@ -5,27 +5,62 @@ Get-Module ProjectTools | Remove-Module -Force
 Import-Module $here\ProjectTools.psm1 -Force
 
 
+Describe 'Error checks on New-Ticket' {
+
+    Context 'Return error messages' {
+
+        It 'Throws error when path does not exist' {
+            { New-Ticket -Ticket "AS-0001" -Path "c:\devdjsfhjk" } | Should -Throw "Path does not exist"
+        } 
+
+        It 'Throws error when ticket folder exists' {
+            { New-Ticket -Ticket "AS-2000" -Path "c:\dev" } | Should -Throw "Ticket folder already exists - use force to create anyway"
+        } 
+
+        It 'Throws error when any ticket folder exists (multiple tickets)' {
+            { New-Ticket -Ticket "AS-1999","AS-2000" -Path "c:\dev" } | Should -Throw "Ticket folder already exists - use force to create anyway"
+        } 
+
+        It 'Returns hash table when ticket folder exists but force is used' {
+            New-Ticket -Ticket "AS-2000" -Path "c:\dev" -Force | Should -BeOfType  System.Collections.Hashtable
+        } 
+
+        It 'Returns hash table for valid input' {
+            New-Ticket -Ticket "AS-0001" -Path "c:\dev" | Should -BeOfType  System.Collections.Hashtable
+        } 
+
+        It 'Returns two hash tables for valid input (multiple tickets)' {
+            New-Ticket -Ticket "AS-1999","AS-1998" -Path "c:\dev" | Should -BeOfType System.Collections.Hashtable
+            New-Ticket -Ticket "AS-1999","AS-1998" -Path "c:\dev" | Should -HaveCount 2
+        } 
+
+
+
+    } 
+} -Tag "error"
+
 Describe 'Tests on New-Ticket' {
 
     Context 'Create all folders' {
 
         $hs = (New-Ticket -Ticket "AS-0001")
 
+
         It 'Should confirm sql files were created' {
             $hs["sql"] | Should -BeExactly "Yes"
-        }
+        } 
 
 
         It 'Should confirm ssis files were created' {
             $hs["ssis"] | Should -BeExactly "Yes"
-        }
+        } -Pending
 
 
         It 'Should confirm test files were created' {
             $hs["test"] | Should -BeExactly "Yes"
-        }
+        } -Pending
 
-    }
+    } 
 
 
     Context 'Create sql folder only' {
@@ -34,7 +69,7 @@ Describe 'Tests on New-Ticket' {
 
         It 'Should confirm sql files were created' {
             $hs["sql"] | Should -BeExactly "Yes"
-        }
+        } -Pending
 
 
         It 'Should confirm ssis files were NOT created' {
@@ -46,7 +81,7 @@ Describe 'Tests on New-Ticket' {
             $hs["test"] | Should -BeExactly "No"
         }
 
-    }
+    } 
 
 
     Context 'Create ssis folder only' {
@@ -60,14 +95,14 @@ Describe 'Tests on New-Ticket' {
 
         It 'Should confirm ssis files were created' {
             $hs["ssis"] | Should -BeExactly "Yes"
-        }
+        } -Pending
 
 
         It 'Should confirm test files were NOT created' {
             $hs["test"] | Should -BeExactly "No"
         }
 
-    }
+    } 
 
 
 
@@ -87,13 +122,13 @@ Describe 'Tests on New-Ticket' {
 
         It 'Should confirm test files were created' {
             $hs["test"] | Should -BeExactly "Yes"
-        }
+        } -Pending
 
-    }
-
-
+    } 
 
 
 
-}
+
+
+} -Tag "Create"
 
